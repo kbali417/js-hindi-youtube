c387d4cfc7458b09e1116fcf151f4fb28e0f7d68
Krishna Balireddy <krishna.balireddy888@gmail.com>
2024-07-27 12:10:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = ""
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// conversation  to Number
+// "33" => 33
+// "33abc" => NAN
+// true => 1 ; false => 0
+//null => 0
+//undefined => NAN
+// ""  => 0
+
+let loggedIn = "krishna"
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn);
+
+//conversion to boolean
+//"" => false
+// 1 => true
+// 0 => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);

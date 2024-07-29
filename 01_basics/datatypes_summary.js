87fdc889ef7f1fa0e609475d03e723f6619c4cd4
Krishna Balireddy <krishna.balireddy888@gmail.com>
2024-07-29 05:28:29 +0000

    Data types summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive Data types
+
+// 7 types : String, Number , boolean, null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+let id = Symbol('123')
+let anotherID = Symbol('123')
+console.log(id === anotherID)
+
+// const bigNumer = 2477358363568395n
+
+
+
+//Reference (Non Primitive)
+
+// Array , Objects, Functions
+
+const arr = ["shaktiman", "naagraj", "doga"];
+
+let obj = {
+    name: "Krishna",
+    age: 25
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof id)
+
+
+
